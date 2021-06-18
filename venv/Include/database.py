9ac833f5508c4225f8baec3ae67c519866d2ce21
romboot <crottyjj@hotmail.com>
2021-06-18 20:53:15 +0100

    made a different database with more tables for each lotto, also added insert functions in the database python file for inserting the results

--- venv/Include/database.py
+++ venv/Include/database.py
@@ -2,13 +2,31 @@ import main
 import sqlite3
 
 from datetime import datetime
-# TODO create functions for each euromillions and irish lotto results, "insert euromillions results in to database", "insert irish lotto plus 1 into database and so on...."
-con = sqlite3.connect("euromillionsResults")
+con = sqlite3.connect("Results")
 
-euroMillionsNumbers = main.getEuroMillionsNumbers()
 date = datetime.today().strftime('%d-%m-%Y')
 
-con.execute("INSERT INTO euromillionsResults VALUES (?, ?, ?, ?, ?, ?, ?, ?);", (date, euroMillionsNumbers[0], euroMillionsNumbers[1], euroMillionsNumbers[2], euroMillionsNumbers[3], euroMillionsNumbers[4], euroMillionsNumbers[5], euroMillionsNumbers[6]))
-con.commit()
-
 # TODO I also need to create more tabels in the database for the different lottos, for example. A table for euromillions irish plus draw, and irish lotto plus 1, lotto plus 2 and so on....
+
+def insertEuromillionsResults():
+    euroMillionsNumbers = main.getEuroMillionsNumbers()
+    con.execute("INSERT INTO euromillionsResults VALUES (?, ?, ?, ?, ?, ?, ?, ?);", (
+    date, euroMillionsNumbers[0], euroMillionsNumbers[1], euroMillionsNumbers[2], euroMillionsNumbers[3],
+    euroMillionsNumbers[4], euroMillionsNumbers[5], euroMillionsNumbers[6]))
+    con.commit()
+
+def insertEuromillionsPlusResults():
+    euroMillionsPlusNumbers = main.getEuroMillionsPlusNumbers()
+    con.execute("INSERT INTO euromillionsPlusResults VALUES (?, ?, ?, ?, ?, ?);", (
+    date, euroMillionsPlusNumbers[0], euroMillionsPlusNumbers[1], euroMillionsPlusNumbers[2], euroMillionsPlusNumbers[3],
+    euroMillionsPlusNumbers[4]))
+    con.commit()
+
+def insertIrishLottoResults():
+    pass
+
+def insertIrishLottoPlus1Results():
+    pass
+
+def insertIrishLottoPlus2Results():
+    pass
