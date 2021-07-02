b49ce0aed03fca504d2f7f48e2946698cebbfaa3
romboot <crottyjj@hotmail.com>
2021-07-02 10:56:41 +0100

    added create_database

--- venv/Include/run_time_script.py
+++ venv/Include/run_time_script.py
@@ -1,7 +1,10 @@
 import schedule
 import time
-
 import database
+import create_database
+
+# Create Database and Tables if they do not already exisits
+create_database.create_tables_if_not_exisits()
 
 def do_euromillions():
     database.insertEuromillionsResults()
@@ -19,6 +22,7 @@ schedule.every().friday.at("21:30").do(do_euromillions)
 #Irish Lotto Results with plus draws
 schedule.every().wednesday.at("21:30").do(do_irishlotto)
 schedule.every().saturday.at("21:30").do(do_irishlotto)
+
 
 while True:
     schedule.run_pending()
