27f45a99575ea7030b038d42ef660a6fa162948b
romboot <crottyjj@hotmail.com>
2021-07-03 16:48:19 +0100

    database creation in run_time_script.py to create database and tables if they don't exist

--- venv/Include/run_time_script.py
+++ venv/Include/run_time_script.py
@@ -1,10 +1,28 @@
 import schedule
 import time
 import database
-import create_database
 
 # Create Database and Tables if they do not already exisits
-create_database.create_tables_if_not_exisits()
+import sqlite3
+
+con = sqlite3.connect("Results")
+cursor = con.cursor()
+
+#Euromillions Table
+cursor.execute("CREATE TABLE IF NOT EXISTS euromillionsResults ( date DATE (10), lottoNum1 INTEGER (2), lottoNum2 INTEGER (2), lottoNum3 INTEGER (2), lottoNum4 INTEGER (2), lottoNum5 INTEGER (2), lottoNum6 INTEGER (2), lottoNum7 INTEGER (2) )")
+#Euromillions Plus Table
+cursor.execute("CREATE TABLE IF NOT EXISTS euromillionsPlusResults ( date DATE (10), lottoNum1 INTEGER (2), lottoNum2 INTEGER (2), lottoNum3 INTEGER (2), lottoNum4 INTEGER (2), lottoNum5 INTEGER (2) )")
+
+
+#irishLotto Table
+cursor.execute("CREATE TABLE IF NOT EXISTS irishLottoResults ( date DATE (10), lottoNum1 INTEGER (2), lottoNum2 INTEGER (2), lottoNum3 INTEGER (2), lottoNum4 INTEGER (2), lottoNum5 INTEGER (2), lottoNum6 INTEGER (2), lottoNum7 INTEGER (2) )")
+#irishLottoPlusOne Table
+cursor.execute("CREATE TABLE IF NOT EXISTS irishLottoPlusOneResults ( date DATE (10), lottoNum1 INTEGER (2), lottoNum2 INTEGER (2), lottoNum3 INTEGER (2), lottoNum4 INTEGER (2), lottoNum5 INTEGER (2), lottoNum6 INTEGER (2), lottoNum7 INTEGER (2) )")
+#irishLottoPlusTwo Table
+cursor.execute("CREATE TABLE IF NOT EXISTS irishLottoPlusTwoResults ( date DATE (10), lottoNum1 INTEGER (2), lottoNum2 INTEGER (2), lottoNum3 INTEGER (2), lottoNum4 INTEGER (2), lottoNum5 INTEGER (2), lottoNum6 INTEGER (2), lottoNum7 INTEGER (2) )")
+con.commit()
+cursor.close()
+con.close()
 
 def do_euromillions():
     database.insertEuromillionsResults()
